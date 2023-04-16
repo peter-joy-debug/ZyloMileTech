02141958d9f4d7849307d8c0080833f53250db19
Peter Joy <ndayishimiyehonore777@gmail.com>
2023-04-16 21:44:45 +0200

    Made different changes

--- MainFrontend/src/app/category-carousel/category-carousel.component.ts
+++ MainFrontend/src/app/category-carousel/category-carousel.component.ts
@@ -14,10 +14,10 @@ export class CategoryCarouselComponent {
   showNavigationArrows = false;
 	showNavigationIndicators = true;
   images = [
-    {title: 'Data Science', short: 'Uncover your potential in Data Science today', src: "https://www.northeastern.edu/graduate/blog/wp-content/uploads/2020/06/iStock-1221293664-1.jpg"},
-    {title: 'Software Engineering', short: 'Uncover your potential in Software Engineering today', src: "https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/634970c7cbeed5644711b937_62fd57ccd6890f25796f92f9_AdobeStock_295461823.jpeg"},
-    {title: 'Machine Learning', short: 'Uncover your potential in Machine Learning today', src: "https://www.bbva.com/wp-content/uploads/2019/11/machine-learning.jpg"},
-    {title: 'Cybersecurity', short: 'Uncover your potential in Cybersecurity today', src: "https://marvel-b1-cdn.bc0a.com/f00000000100045/www.elmhurst.edu/wp-content/uploads/2020/03/cybersecurity-vs-information-security-illustration.jpg"}
+    {title: 'Technology', short: 'Uncover best Tech-based projects today', src: "https://www.northeastern.edu/graduate/blog/wp-content/uploads/2020/06/iStock-1221293664-1.jpg"},
+    {title: 'Agriculture', short: 'Uncover best Agri-based projects today', src: "https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/634970c7cbeed5644711b937_62fd57ccd6890f25796f92f9_AdobeStock_295461823.jpeg"},
+    {title: 'Trading', short: 'Uncover best Trading projects today', src: "https://www.bbva.com/wp-content/uploads/2019/11/machine-learning.jpg"},
+    {title: 'Education', short: 'Uncover the best Edu-based projects today', src: "https://marvel-b1-cdn.bc0a.com/f00000000100045/www.elmhurst.edu/wp-content/uploads/2020/03/cybersecurity-vs-information-security-illustration.jpg"}
   ];
    
 
